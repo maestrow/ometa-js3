b6adce0548cb053128f4c71585d2ab691ef8a107
maestrow <andrew.shmelev@gmail.com>
2021-05-12 17:55:39 +0300

    add typescript

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const getPath = (relative) => path.join(__dirname, relative);
+
+module.exports = env => ({
+  context: getPath(''),
+  entry: {
+    main: env.src
+  },
+  devtool: "source-map",
+  resolve: {
+    alias: {
+      src: getPath('src')
+    },
+    extensions: ['.ts', '.js'],
+    plugins: [
+      new TsconfigPathsPlugin({ configFile: getPath("tsconfig.json") })
+    ]
+  },
+  output: {
+    path: getPath('dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader',
+      },
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+  ]
+});
