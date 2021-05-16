917cf9dff126a324a009100eef6c1e96ff625f80
maestrow <andrew.shmelev@gmail.com>
2021-05-16 19:31:56 +0300

    Replace webpack with esbuild. Fix VSCode debugging when fs module is used. Add test.

--- src/step2/test.ts
+++ src/step2/test.ts
@@ -1,7 +1,11 @@
-test('the data is peanut butter', () => {
-  expect(1).toBe(1)
-});
+import { Parser } from "./parser";
+import { ometa1, proj } from './grammars/ometa1'
+import * as fs from "fs";
 
-test('greeting', () => {
-  expect(1).toBe(1)
+test('the data is peanut butter', () => {
+  const input = fs.readFileSync('src/step2/grammars/ometa1.ometa', 'utf-8')
+  const p = new Parser(ometa1, input as unknown as any[], proj)
+  const r = p.match('ometa')
+  //const p = new Parser()
+  expect(r.success).toBe(true)
 });
