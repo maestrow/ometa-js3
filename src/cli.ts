b9d99e8b5e542b0f91009e60272e30e001f364a0
maestrow <andrew.shmelev@gmail.com>
2021-05-17 22:37:10 +0300

    cli

--- src/cli.ts
+++ src/cli.ts
@@ -5,19 +5,19 @@ import { math1 } from './grammars/math1'
 import { ometa1, proj } from './grammars/ometa1'
 import { TraceDiscovery } from "./utils/trace-discover";
 
+const saveTrace = (p: Parser) => {
+  const disco = new TraceDiscovery(ometa1, input, p.trace.data)
+  const trace = disco.convert()
+  fs.writeFileSync("./dist/trace.json", JSON.stringify(trace, null, 2))  
+}
 
 const input = fs.readFileSync('src/grammars/ometa1.ometa', 'utf-8')
 
-//const p = new Parser(math1, [...'((1+2)-3*3)/4'])
-//const r = p.match('expr')
-
 const p = new Parser(ometa1, input as unknown as any[], proj)
+
 const r = p.match('ometa')
 
 
 console.dir(r, {depth: null})
-const disco = new TraceDiscovery(ometa1, input, p.trace.data)
-const trace = disco.convert()
 
-//console.dir(trace, {depth: null})
-fs.writeFileSync("./dist/trace.json", JSON.stringify(trace, null, 2))
+
